ce0861678f6ed2cb923aac05206012c17231a3e2
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2022-10-24 16:17:43 +0700

    fixed interceptor

--- src/context/interceptor.js
+++ src/context/interceptor.js
@@ -4,7 +4,12 @@ const axiosApiInstance = axios.create({});
 
 axiosApiInstance.interceptors.request.use((config) => {
   let tokensData = JSON.parse(localStorage.getItem("tokens"));
-  config.headers = { 
+  if(tokensData === null){
+    localStorage.clear()
+    window.location.href = "/login";
+  }
+
+  config.headers = {
     'Authorization': `Bearer ${tokensData.data.accessToken}`,
     'Accept': 'application/json',
     'Content-Type': 'application/x-www-form-urlencoded'
@@ -19,21 +24,29 @@ axiosApiInstance.interceptors.response.use(
   async (error) => {
     console.log(error.response)
     if (error.response.status === 401) {
+      alert("Access da het han")
       const authData = JSON.parse(localStorage.getItem("tokens"));
       const payload = {
         accessToken: authData.data.accessToken,
         refreshToken: authData.data.refreshToken,
       };
- 
-      let apiResponse = await axios.post(
-        axios.defaults.baseURL + "​/api​/auth​/user​/refresh",
-        payload
+      let apiResponse = await axios.get(
+        axios.defaults.baseURL + `/api/auth/user/refresh/${authData.data.refreshToken}`
       );
+      console.log(apiResponse.data.data)
+      if(apiResponse.data.data.status === false){
+        localStorage.clear()
+        window.location.href = "/login";
+        alert("Refresh da het han")
+        return Promise.reject(error);
+      }
+      alert("Da Refresh Token")
       localStorage.setItem("tokens", JSON.stringify(apiResponse.data));
       error.config.headers[
         "Authorization"
       ] = `Bearer ${apiResponse.data.accessToken}`;
       return axios(error.config);
+
     } else {
       return Promise.reject(error);
     }
