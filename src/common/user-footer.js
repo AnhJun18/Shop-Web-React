418fe95a528b6bcca3e075524d4bf97c9f2c8bd6
jinnguyen0612 <devuongbongdem@gmail.com>
2022-11-11 08:17:40 +0700

    User-View
    without Cart page

---
+++ src/common/user-footer.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+class UserFooter extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return 	<footer id="footer">
+		<footer class="bg-dark" id="tempaltemo_footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-success border-bottom pb-3 border-light logo">Zay Shop</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li>
+                            <i class="fas fa-map-marker-alt fa-fw"></i>
+                            123 Consectetur at ligula 10660
+                        </li>
+                        <li>
+                            <i class="fa fa-phone fa-fw"></i>
+                            <a class="text-decoration-none" href="tel:010-020-0340">010-020-0340</a>
+                        </li>
+                        <li>
+                            <i class="fa fa-envelope fa-fw"></i>
+                            <a class="text-decoration-none" href="mailto:info@company.com">info@company.com</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Products</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><a class="text-decoration-none" href="#">Luxury</a></li>
+                        <li><a class="text-decoration-none" href="#">Sport Wear</a></li>
+                        <li><a class="text-decoration-none" href="#">Men's Shoes</a></li>
+                        <li><a class="text-decoration-none" href="#">Women's Shoes</a></li>
+                        <li><a class="text-decoration-none" href="#">Popular Dress</a></li>
+                        <li><a class="text-decoration-none" href="#">Gym Accessories</a></li>
+                        <li><a class="text-decoration-none" href="#">Sport Shoes</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Further Info</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><a class="text-decoration-none" href="#">Home</a></li>
+                        <li><a class="text-decoration-none" href="#">About Us</a></li>
+                        <li><a class="text-decoration-none" href="#">Shop Locations</a></li>
+                        <li><a class="text-decoration-none" href="#">FAQs</a></li>
+                        <li><a class="text-decoration-none" href="#">Contact</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+
+    </footer>
+		
+	</footer>
+    }
+}
+
+export default UserFooter;
---
+++ src/user/userLayout.js
@@ -0,0 +1,70 @@
+import React from "react";
+import UserHeader from './../common/user-header';
+import UserFooter from './../common/user-footer';
+import { Preloader, Bars } from 'react-preloader-icon';
+
+const userLayout = (ChildComponent) => {
+    class UserLayout extends React.Component {
+        constructor(props){
+            super(props);
+    
+            this.state = {
+                pageLoaded: false,
+                saveLeadClickEvent: ""
+            };
+        }
+
+        componentDidMount(){
+            setTimeout(() => {
+                this.setState(() => ({
+                    pageLoaded: true
+                }))
+            }, 1000);
+        }
+
+        renderHtml(){
+            if(!this.state.pageLoaded){
+                return <div className="loading-page">
+                    <div className="center">
+                        <Preloader use={Bars} size={60} strokeWidth={10} strokeColor="#f7b085" duration={600} />
+                    </div>
+              </div>
+            }
+
+            return <div className="" id="">
+                {/* <!-- Page content wrapper--> */}
+                <div id="page-content-wrapper">
+                    {/* <!-- Top navigation--> */}
+                    <UserHeader />
+                    {/* <!-- Page content--> */}
+                    <div className="container-fluid content-container">
+                        <ChildComponent {...this.props} />
+                    </div>
+                    <UserFooter />
+                </div>
+            </div>
+        }
+
+        addLeadModalFooterContent(){ 
+            return <>
+                <div style={{width:"100%"}}>
+                    <button onClick={(e) => this.setState(() => ({saveLeadClickEvent: (Math.random() + 1).toString(36).substring(7)}))} className="btn btn-default low-height-btn">Add Lead</button> 
+                </div>
+            </>;
+        }
+
+        handleParentData = (e) => {
+            console.log(e);
+        }
+
+        render(){
+            return <>
+                {this.renderHtml()}
+            </>
+        }
+    }
+
+    return UserLayout;
+}
+
+export default userLayout;
