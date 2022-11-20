39dc4b0d08371c263ad0fa37de52b19a8aeecd2f
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2022-11-20 09:31:12 +0700

    fixed not show shop in cart page

--- src/common/user-header.js
+++ src/common/user-header.js
@@ -57,7 +57,7 @@ class UserHeader extends React.Component {
                     <a class="nav-icon d-none d-lg-inline" href="#" data-bs-toggle="modal" data-bs-target="#templatemo_search">
                         <i class="fa fa-fw fa-search text-dark mr-2"></i>
                     </a>
-                    <a class="nav-icon position-relative text-decoration-none padding-cart" href="#">
+                    <a class="nav-icon position-relative text-decoration-none padding-cart" href="/cart">
                         <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
                         <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-cart-light text-dark">7</span>
                     </a>
