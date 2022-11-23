2f7252422a085800b94b6006ec06853435e5a308
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2022-11-23 15:58:21 +0700

    api submit order to server

--- src/pages/TheOrderPage.js
+++ src/pages/TheOrderPage.js
@@ -7,6 +7,7 @@ import userLayout from "../user/userLayout"
 import {useLocation, useNavigate} from "react-router-dom";
 import axiosApiInstance from "../context/interceptor";
 import axios from "../api/axios";
+import {toast} from "react-toastify";
 
 /*var carts = '';
 const Cart = (cart) => {
@@ -22,10 +23,23 @@ const TheOrderPage = () => {
     const cart = location.state;
     const [load, setLoad] = useState(false)
     const [profile, setProfile] = useState({})
-    const [total, setTotal] = useState(10000)
     const [province, setProvince] = useState([])
     const [districts, setDistricts] = useState([])
     const [wards, setWards] = useState([])
+    const [feeShip, setFeeShip] = useState(0)
+    const [tmpMoney, setMoney] = useState(0)
+    const [nameReceiver, setName] = useState()
+    const [phoneReceiver, setPhone] = useState()
+    const [address, setAddress] = useState()
+
+    useEffect(()=>{
+        let t=0
+        cart.forEach((i)=>{
+            t += i?.amount * i.product.infoProduct.price
+        })
+        setMoney(t)
+    },[])
+
     const [order, setOrder] = useState({
         "pick_province": "Thành Phố Hồ Chí Minh",
         "pick_district": "Quận 9",
@@ -36,12 +50,14 @@ const TheOrderPage = () => {
         "value": 0,
     })
 
-
     async function getProfile() {
         const result = await axiosApiInstance.get(axiosApiInstance.defaults.baseURL + `/api/user/profile`);
         setLoad(true)
-        setProfile(result?.data?.data?.userInfo)
-
+        const data=result?.data?.data?.userInfo
+        setProfile(data)
+        setName(data?.firstName ? data.firstName : '' + ' ' + data.lastName ? data.lastName : '')
+        setPhone(data?.phone)
+        setAddress(data?.address)
     }
 
     async function getProvince() {
@@ -82,6 +98,18 @@ const TheOrderPage = () => {
         console.log(result)
 
     }
+    const changeName = (e) => {
+        setName(e.target.value)
+    }
+
+    const changePhone = (e) => {
+        setPhone(e.target.value)
+    }
+
+    const changeAddress = (e) => {
+        setAddress(e.target.value)
+    }
+
     const changeProvince = (e) => {
         setDistricts(province.find(i => i.name === e.target.value).districts)
         order.province = e.target.value
@@ -93,6 +121,35 @@ const TheOrderPage = () => {
         order.district = e.target.value
         setOrder(order)
         getFeeShip()
+    }
+    const changeWard = (e) => {
+
+        order.address = e.target.value
+        setOrder(order)
+    }
+
+    const handleConfirmOrder = async () => {
+        const productOrder = []
+        cart.forEach((i) => {
+            productOrder.push({"product_id": i?.product?.id, "amount": i.amount})
+        })
+        const payload = {
+            "nameReceiver": nameReceiver,
+            "address": `${address}, ${order.address}, ${order.district}, ${order.province}`,
+            "phoneReceiver": phoneReceiver,
+            "feeShip": feeShip,
+            "note": "none",
+            "listProduct": productOrder
+        }
+        const result = await axiosApiInstance.post(axiosApiInstance.defaults.baseURL + `/api/order/create`,payload)
+        if(result?.data.status){
+            toast.success("Đơn hàng đã được tạo")
+            navigate('/home');
+        }
+        else {
+            toast.success("Vui lòng kiểm tra thông tin! "+result?.data.message)
+        }
+
     }
     useEffect(() => {
         getProfile();
@@ -106,28 +163,21 @@ const TheOrderPage = () => {
                     <div className="field field_v1 ms-4">
                         <label for="first-name" className="ha-screen-reader">Họ tên</label>
                         <input id="nameReceiver"
-                               defaultValue={profile.firstName ? profile.firstName : '' + ' ' + profile.lastName ? profile.lastName : ''}
-                               className="field__input" placeholder=" "></input>
+                               defaultValue={nameReceiver}
+                          onChange={changeName}     className="field__input" placeholder=" "></input>
                         <span className="field__label-wrap" aria-hidden="true">
                             <span className="field__label">Họ tên</span>
                         </span>
                     </div>
                     <div className="field field_v1 ms-4">
                         <label for="first-name" className="ha-screen-reader">Số điện thoại</label>
-                        <input id="phoneReceiver" defaultValue={profile?.phone} className="field__input"
+                        <input id="phoneReceiver" defaultValue={phoneReceiver} className="field__input" onChange={changePhone}
                                placeholder=" "></input>
                         <span className="field__label-wrap" aria-hidden="true">
                             <span className="field__label">Số điện thoại</span>
                         </span>
                     </div>
-                    <div className="field field_v1 ms-4 mb-2">
-                        <label for="first-name" className="ha-screen-reader">Địa chỉ</label>
-                        <input id="address" className="field__input" defaultValue={profile?.address}
-                               placeholder=" "></input>
-                        <span className="field__label-wrap" aria-hidden="true">
-                            <span className="field__label">Địa chỉ</span>
-                        </span>
-                    </div>
+
                     <div className="row mb-3">
                         <div className="field field_v1 ms-4 col">
                             <label className="ha-screen-reader">Tỉnh/thành</label>
@@ -151,7 +201,7 @@ const TheOrderPage = () => {
                         </div>
                         <div className="field field_v1 ms-3 col">
                             <label className="ha-screen-reader">Phường/xã</label>
-                            <select className="field__input" placeholder=" ">
+                            <select className="field__input" placeholder=" " onChange={changeWard}>
                                 <option value="">Chọn Phường/xã</option>
                                 {wards?.map(i => <option>{i.name}</option>)}
                             </select>
@@ -159,6 +209,14 @@ const TheOrderPage = () => {
                                 <span className="field__label">Phường/xã</span>
                             </span>
                         </div>
+                    </div>
+                    <div className="field field_v1 ms-4 mb-2">
+                        <label htmlFor="first-name" className="ha-screen-reader">Địa chỉ</label>
+                        <input id="address" className="field__input" defaultValue={address} onChange={changeAddress}
+                               placeholder=" "></input>
+                        <span className="field__label-wrap" aria-hidden="true">
+                            <span className="field__label">Địa chỉ</span>
+                        </span>
                     </div>
                     <h5 className="ms-4 mb-3 mt-1">Phương thức vận chuyển</h5>
 
@@ -172,7 +230,7 @@ const TheOrderPage = () => {
 
                     </div>
                     <div className="shopping-cart-footer">
-                        <div><a className="btn btn-outline-secondary mb-4" href="/cart"><i
+                        <div><a className="btn btn-outline-secondary mt-2 w-25 mb-2" href="/cart"><i
                             className="icon-arrow-left"></i>&nbsp;Giỏ hàng</a></div>
                     </div>
                 </div>
@@ -204,24 +262,34 @@ const TheOrderPage = () => {
                     <table className="table">
                         <tbody>
                         <tr>
-                            <td>Tạm tính:</td>
-                            <td className="textAlign"><span>19000</span></td>
+                            <td>Tạm tính </td>
+                            <td className="textAlign"><span>{tmpMoney.toLocaleString('vi', {
+                                style: 'currency',
+                                currency: 'VND'
+                            })}</span></td>
                         </tr>
                         <tr>
-                            <td>Phí ship:</td>
-                            <td className="textAlign"><span>19000</span></td>
+                            <td>Phí ship </td>
+                            <td className="textAlign"><span>{feeShip.toLocaleString('vi', {
+                                style: 'currency',
+                                currency: 'VND'
+                            })}</span></td>
                         </tr>
                         </tbody>
                         <tfoot>
                         <tr>
-                            <td>Tổng tiền:</td>
-                            <td className="textAlign"><span>38000 VND</span></td>
+                            <td>Tổng tiền </td>
+                            <td className="textAlign"><span>{(tmpMoney+feeShip).toLocaleString('vi', {
+                                style: 'currency',
+                                currency: 'VND'
+                            })}</span></td>
                         </tr>
                         </tfoot>
                     </table>
                     <div className="shopping-cart-footer">
-                        <div className="column">
-                            <a className="btn btn-success" href="#"> Đặt hàng </a></div>
+                        <div className="col-10 mt-3 mb-3 m-auto">
+                            <button className="btn btn-success w-100" onClick={handleConfirmOrder}> Đặt hàng </button>
+                        </div>
                     </div>
                 </div>
             </div>
