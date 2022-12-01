0b8cb724340864f2b7a72c70147bc1d50e832c77
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2022-12-01 12:56:43 +0700

    chat box api

--- src/common/chatbox.js
+++ src/common/chatbox.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import './../assets/css/chatbox.css'
+import axios from "../api/axios";
 
 
 
@@ -16,9 +17,13 @@ const ChatBox = ()=> {
         }
     }
 
-    const callChatBox=()=>{
-        const kq={
-            response:'...'
+    const callChatBox=async (msg) => {
+        const response =await axios(`http://127.0.0.1:5000/get?msg=${msg}`, {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'no-cors', // no-cors, *cors, same-origin
+        })
+        const kq = {
+            response: response.data
         }
         talk.push(kq)
         setTalk(talk)
@@ -29,33 +34,33 @@ const ChatBox = ()=> {
        setShow(!show);
     }
 
-    const handleSend = (e)=>{
-       if(message){
-           const tmp={
-               request:message
-           }
-           talk.push(tmp)
-           setTalk(talk)
-           callChatBox()
-           setMessage("")
-       }
+    const handleSend = async (e) => {
+        if (message) {
+            const tmp = {
+                request: message
+            }
+            talk.push(tmp)
+            setTalk(talk)
+            await callChatBox()
+            setMessage("")
+        }
 
     }
 
-    const handleSubmit = (e)=>{
-       if(e.key ==="Enter" && message){
-           const tmp={
-               request:message
-           }
-           talk.push(tmp)
-           setTalk(talk)
-           callChatBox()
-           setMessage("")
-       }
+    const handleSubmit = async (e) => {
+        if (e.key === "Enter" && message) {
+            const tmp = {
+                request: message
+            }
+            talk.push(tmp)
+            setTalk(talk)
+            await callChatBox(message)
+            setMessage("")
+        }
     }
     useEffect(()=>{
         scroll();
-    },[message])
+    },[message,show])
 
     return <>
             <div className="chat-button ml-3 mb-4">
@@ -64,43 +69,40 @@ const ChatBox = ()=> {
           {show ?
               <div class="container chat-container d-flex justify-content-center" >
                 <div class="card chat-card ">
-                    <div class="chat-header d-flex flex-row justify-content-between p-3 adiv text-white">
-                        <span class="pb-3">Trợ lý ảo PTIT</span>
-                        <i onClick={handleShow} class="close fa fa-times"></i>
+                    <div className=" chat-header d-flex flex-row  adiv ">
+                        <img src={require('./../assets/images/chatbox.png')} width="50" height="50"/>
+                        <div className="d-flex justify-content-between p-3 w-100 ps-0 text-white">
+                            <span className="pb-3">ChatBox tư vấn</span>
+                            <i onClick={handleShow} className="close fa fa-times"></i>
+                        </div>
                     </div>
+
                     <div class="chat-element"  id="chat-feed">
                         <div class="d-flex p-3">
-                            <img src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png/" width="30" height="30"/>
-                            <div class="chat-response ml-2 p-3">Chào bạn, chúc bạn một ngày tốt lành ❤ <br/>
-                                Tôi là trợ lý ảo của Shop PTIT. Hãy hỏi để tôi có thể giúp bạn</div>
+                            <img src={require('./../assets/images/chatbox.png')}  width="30" height="30"/>
+                            <div class="chat-response ml-2 p-3 text-justify">Chào bạn, mình là chatbot tư vấn ❤! <br/>
+                                Bạn có thể cho mình biết bạn muốn mua đồ đi đâu được không? </div>
                         </div>
 
                   {talk.map((i)=>
                       i.request?
                           <div className="d-flex  flex-row-reverse p-3">
-                              <img src="https://img.icons8.com/color/48/000000/circled-user-male-skin-type-7.png/"
-                                   width="30" height="30"/>
-                              <div className="chat-request  mr-2 p-3"><span>{i.request}</span>
+                              <div className="chat-request  mr-2 p-3 text-justify"><span>{i.request}</span>
                               </div>
                           </div>
                           :
                           i.response?
                               <div className="d-flex p-3">
-                                  <img src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png/"
+                                  <img src={require('./../assets/images/chatbox.png')}
                                        width="30" height="30"/>
                                   <div className="chat-response ml-2 p-3">{i.response}
                                   </div>
                               </div>
                               :null
                   )     }
-
-                     {/*    <div class="d-flex flex-row p-3">
-                            <img src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png" width="30" height="30"/>
-                            <div class="chat ml-2 p-3"><span class="text-muted dot">. . .</span></div>
-                        </div>*/}
                     </div>
                     <div class=" d-flex flex-row align-items-center px-3 chatbox-input">
-                        <input id="txtChatText" autoComplete="off" className="cssChatText" value={message} onKeyDown={(e)=>handleSubmit(e)} onChange={(e)=>setMessage(e.currentTarget.value)} placeholder="Nhập câu hỏi của bạn..."/>
+                        <input id="txtChatText" autoComplete="off" className="cssChatText text-justify" value={message} onKeyDown={(e)=>handleSubmit(e)} onChange={(e)=>setMessage(e.currentTarget.value)} placeholder="Nhập câu hỏi của bạn..."/>
                         <i className="fa fa-paper-plane icon-send" onClick={handleSend}></i>
                     </div>
                 </div>
