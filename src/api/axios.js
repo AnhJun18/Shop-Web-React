cacaa546821a65c1568d1a17af2050e39239d101
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2023-04-22 08:02:22 +0700

    change server to railway

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: 'http://localhost:8081',
+	baseURL: 'https://pa-shop-api.up.railway.app',
 
 	// baseURL: 'https://pa-shop-service.azurewebsites.net',
 });
